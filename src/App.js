9086a614134f2f6446247fa0dc18c31c9e436b31
SAUMYA SINGH <saumya0234.cse19@chitkara.edu.in>
2022-01-26 22:12:25 +0530

    ip tracker

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+import "leaflet/dist/leaflet.css";
+import InputForm from "./InputForm";
+export default function App() {
+  return (
+    <div className="App">
+      <InputForm />
+    </div>
+  );
+}
